ca5a7f311139b00bae64cb5defa240cb1521fc80
Sudhi Kumaran <sudhikumaran2005@gmail.com>
2025-12-01 17:20:50 +0530

    Double rent problem solved

---
+++ Double Rent.py
@@ -0,0 +1,19 @@
+"""Double Rent
+Chefina decided to move into Chef's apartment.
+Chef was initially paying a rent of 
+X
+X rupees. Since Chefina is moving in, the owner decided to double the rent.
+
+Find the final rent Chef needs to pay.
+
+Input Format
+The input consists of a single integer 
+X
+X, denoting the rent Chef was initially paying.
+
+Output Format
+Output on a new line, the final rent Chef needs to pay."""
+
+x = int(input())
+new_rent = 2 * x
+print(new_rent)

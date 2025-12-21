5d55b19f1fd525567c5b48f248ae08d814dca4a8
Sudhi Kumaran <sudhikumaran2005@gmail.com>
2025-12-21 11:18:19 +0530

    Problem solved

---
+++ removeduplicates.py
@@ -0,0 +1,34 @@
+"""Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+
+The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted."""
+
+nums = list(map(int,input().split()))
+
+if not nums:
+    print(0)
+
+k = 1
+
+for i in range(1,len(nums)):
+    if nums[i] != nums[i-1]:
+        nums[k] = nums[i]
+        k+=1
+print(k)
+print(nums)
